a04ec34c28bdfead07460b2b45a3521d566b7178
Verdysh Volodymyr <verdyshvladimir@gmail.com>
2025-04-04 11:42:12 +0300

    fixed commet AI

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,17 @@
-export const Person = ({ person }) => {
-  let sex = 'wife';
+export const Person = ({ name, sex, age, isMarried, partnerName}) => {
+  let male = 'husband';
 
-  if (person.sex === 'f') {
-    sex = 'husband';
+  if (sex === 'm') {
+    male = 'wife';
   }
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age > 0 ? <p className="Person__age">I am {person.age}</p> : ''}
-      {person.isMarried === true ? (
+      <h2 className="Person__name">My name is {name}</h2>
+      {age > 0 ? <p className="Person__age">I am {age}</p> : ''}
+      {isMarried === true ? (
         <p className="Person__partner">
-          {person.partnerName} is my {sex}
+          {partnerName} is my {male}
         </p>
       ) : (
         <p className="Person__partner">I am not married</p>
